83d10947c9a2319aea1e4b7a6745c8b490a64107
Andy <89816884+lya0108@users.noreply.github.com>
2024-02-09 10:23:48 +1300

    Add files via upload

--- 01_Converter_Gui_v1.py
+++ 01_Converter_Gui_v1.py
@@ -9,15 +9,18 @@ class converter:
         self.temp_frame = Frame(padx=10, pady=10)
         self.temp_frame.grid()
 
-        self.temp_heading = Label(self.temp_frame, text="Temperature Converter", font=("Arial", "18", "Bold"))
+        self.temp_heading = Label(self.temp_frame, text="Temperature Converter", font=("Arial", "18", "bold"))
         self.temp_heading.grid(row=0, column=0)
 
-        instructions = "Please Enter a Temperature Below and Then"
+        instructions = "Please Enter a Temperature Below and Then Press One of The Buttons to Convert it From Centigrade to Fahrenheit."
+
+        self.temp_instructions = Label(self.temp_frame, text=instructions, wrap=250, width=40, justify="left")
+        self.temp_instructions.grid(row=1, column=0)
 
 # main routine
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("Temperature Converter")
-    root.geometry("512x512")
+    root.geometry("310x512")
     converter()
     root.mainloop()
